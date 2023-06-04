aad9231489dc39ba76f9bf41d63d05c9b4f7770d
Hericles <hericles_003@hotmail.com>
2023-06-04 19:08:07 -0300

    feat: create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+
+export const Header = ({ children }: { children: ReactNode }) => {
+    return (
+        <header className="flex justify-between border-b-2 border-b-[#DEE2E6]">
+            <figure className="ml-[60px] my-[26.83px]">
+                <Image
+                    src="/MotorShopLogo.png"
+                    width={153}
+                    height={26}
+                    alt="Motor Shop"
+                />
+            </figure>
+            <div className="border-l-2 border-l-[#DEE2E6] pl-[44px] pr-[60px] flex items-center">
+                {children}
+            </div>
+        </header>
+    );
+};
