ffb30110ddf6a99a5bb4882fadb6eb55db036b3a
Leonaro-L-Cunha <leonavidareal@gmail.com>
2023-06-30 00:59:03 -0300

    fix:css responsive

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,16 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { GiHamburgerMenu} from "react-icons/gi"
+import { AiOutlineClose} from "react-icons/ai"
 
 export const Header = ({ children }: { children?: ReactNode }) => {
+    const [hamburger, setHamburger] = useState(true)
     return (
-        <header className="flex justify-between border-b-2 border-b-[#DEE2E6]" id="header-top">
+        <header className="flex justify-between items-center p-4 border-b-2 border-b-[#DEE2E6]" id="header-top">
             <Link href={'/'}>
-                <figure className="ml-[60px] my-[26.83px]">
+                <figure>
                     <Image
                         src="/assets/MotorShopLogo.png"
                         width={153}
@@ -15,7 +19,15 @@ export const Header = ({ children }: { children?: ReactNode }) => {
                     />
                 </figure>
             </Link>
-            <div className="border-l-2 border-l-[#DEE2E6] pl-[44px] pr-[60px] flex items-center">
+            <div className="flex flex-col relative lg:hidden">
+                <button onClick={()=> setHamburger(!hamburger)}>{ hamburger ?<GiHamburgerMenu/> : <AiOutlineClose/> }</button>
+                {!hamburger &&
+                 <div className="w-[257px] absolute flex flex-col left-[-226px] top-[39px] z-50 bg-white p-4 gap-4">
+                    {children}
+                 </div>
+                }
+            </div>
+            <div className="border-l-2 border-l-[#DEE2E6] pl-[44px] pr-[60px]  items-center hidden lg:flex">
                 {children}
             </div>
         </header>
